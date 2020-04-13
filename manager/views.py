05182cb8b49050ba5feb62d6edd7dd9e721732af
MD Sayem <mdsayem1629@gmail.com>
2020-04-13 22:57:33 +0600

    T 44 and 4-6

--- manager/views.py
+++ manager/views.py
@@ -44,3 +44,32 @@ def manager_group_del(request, name):
     b.delete()
 
     return redirect('manager_group')
+
+
+def users_groups(request, pk):
+    manager = Manager.objects.get(pk=pk)
+    user = User.objects.get(username=manager.utxt)
+    ugroup = []
+    for i in user.groups.all():
+        ugroup.append(i.name)
+
+    group = Group.objects.all()
+    return render(request, 'back/users_groups.html', {'ugroup': ugroup, 'group': group, 'pk': pk})
+
+
+def add_users_to_groups(request, pk):
+    if request.method == 'POST':
+        gname = request.POST.get('gname')
+        group = Group.objects.get(name=gname)
+        manager = Manager.objects.get(pk=pk)
+        user = User.objects.get(username=manager.utxt)
+        user.groups.add(group)
+    return redirect('users_groups', pk=pk)
+
+
+def del_users_to_groups(request, pk, name):
+    group = Group.objects.get(name=name)
+    manager = Manager.objects.get(pk=pk)
+    user = User.objects.get(username=manager.utxt)
+    user.groups.remove(group)
+    return redirect('users_groups', pk=pk)
